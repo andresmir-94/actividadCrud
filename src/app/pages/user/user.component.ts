8fa44aae8f18e58f41a8d4c7428a57401e102a37
andresmir-94 <andresmirsampedro@gmail.com>
2025-03-23 18:02:20 +0100

    Criterio 5
    Creación de la vista del componente//userComponent

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject,Input } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import { RouterLink } from '@angular/router';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+@Input() idUser: string = '';
+@Input() theUser: IUser | any 
+usersServices= inject(UsersService);
+
+
+
+
+
+
+async ngOnInit() {
+  let _id = this.idUser;
+  try{
+    this.theUser = await this.usersServices.getById(_id);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+deleteUser(_id: string) {
+  toast.error(`Deseas borrar al usuario ${this.theUser?.first_name}?`, 
+    {
+    action: {
+      label: 'Aceptar',
+      onClick: async () => {
+        
+        try{
+          let response= await this.usersServices.delete(_id)
+          window.location.href= '/users';
+          console.log('Usuario eliminado', response);
+        }catch (error) {
+          console.log('Error al eliminar el usuario', error)
+        }
+      }
+    }
+  });
+  
+}
 
 }
