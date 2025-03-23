8a459b968bf90b26fb9720edbc9a204675e60d50
andresmir-94 <andresmirsampedro@gmail.com>
2025-03-23 16:45:44 +0100

    Criterio 1 y 2 de la actividad
    Creacion de rutas y componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { FormUserComponent } from './pages/form-user/form-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'user/:idUser', component: UserComponent},
+    {path: 'newuser', component: FormUserComponent},
+    {path: 'updateuser/:idUser', component: FormUserComponent},
+    {path: '**', redirectTo: 'home'}
+];
