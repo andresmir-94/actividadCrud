4f606d437a72f4136e9fc3e542dbc78e477fa511
andresmir-94 <andresmirsampedro@gmail.com>
2025-03-23 16:58:11 +0100

    Criterio 2
    Creacion del servicio

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { IResponse } from '../interfaces/iresponse.interface';
+import { IUser } from '../interfaces/iuser.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+private httpClient = inject(HttpClient);
+private baseUrl: string = 'https://peticiones.online/api/users';
+
+getAllObservable(page:number): Observable<IResponse> {
+  return this.httpClient.get<IResponse>(`${this.baseUrl}?page=${page}`);
+}
+
+getById(_id: string): Promise<IUser> {
+  return lastValueFrom(this.httpClient.get<IUser>(`${this.baseUrl}/${_id}`));
+}
+
+delete(_id: string): Promise<IUser> {
+  return lastValueFrom(this.httpClient.delete<IUser>(`${this.baseUrl}/${_id}`));
+ }
+update(user: IUser): Promise<IUser> {
+
+  return lastValueFrom(this.httpClient.put<IUser>(`${this.baseUrl}/${user._id}`, user));}
+
+insert(user: IUser){
+ return lastValueFrom(this.httpClient.post<IUser>(this.baseUrl, user));
+}
+
 }
